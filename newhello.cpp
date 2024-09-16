9ff4ca176545076aca393d7467fac78d2eabd12a
vboxuser <vboxuser@UBUNTU.myguest.virtualbox.org>
2024-09-16 19:10:55 +0300

    написанная и рабочая функция newhello в одноименном файле

---
+++ newhello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int say_hello() {
+	using namespace std;
+	cout << "Hello, world!" << endl;
+	while (true) {
+		string x;
+		cin >> x;
+		cout << "Hello, " << x << "!" << endl;
+	}
+	return 0;
+}
